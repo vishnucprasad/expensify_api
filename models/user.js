dd35e3c6f0735a6723d10af2bcdcc7e3bac6644e
Vishnu C Prasad <mail@vishnucprasad.in>
2022-07-28 17:58:36 +0530

    setup user model

---
+++ models/user.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: [true, 'You must provide your first name'],
+        trim: true,
+        minlength: [3, 'Must be at least 4 charecters, got {VALUE}!'],
+    },
+    lastName: {
+        type: String,
+        required: [true, 'You must provide your first name'],
+        trim: true,
+        minlength: [2, 'Must be at least 2 charecters, got {VALUE}!'],
+    },
+    email: {
+        type: String,
+        required: [true, 'You must provide an email!'],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'You must provide a password'],
+    }
+});
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+    const hash = await bcrypt.hash(user.password, 10);
+    user.password = hash;
+    next();
+});
+
+userSchema.methods.toJSON = function () {
+    const user = this.toObject();
+    delete user.password;
+
+    return user;
+}
+
+userSchema.methods.isValidPassword = async function (password) {
+    const user = this;
+
+    return await bcrypt.compare(password, user.password);
+}
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
