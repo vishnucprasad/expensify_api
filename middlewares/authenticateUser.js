a94105fd9ff6701e425335b26e0a8b75b3deb853
Vishnu C Prasad <mail@vishnucprasad.in>
2022-08-15 11:12:43 +0530

    Modified authentication middleware

--- middlewares/authenticateUser.js
+++ middlewares/authenticateUser.js
@@ -1,7 +1,17 @@
 const passport = require("passport");
+const UnauthorizedError = require("../errors/unAuthorizedError");
 
 const authenticateUser = (req, res, next) => {
-    passport.authenticate('jwt', { session: false },)(req, res, next);
+    try {
+        passport.authenticate('jwt', { session: false }, (e, user) => {
+            if (e) if (!user) throw new UnauthorizedError();
+            if (!user) throw new UnauthorizedError();
+
+            return res.json({ ...user, authToken: req.headers.authorization.split(" ")[1] })
+        })(req, res, next);
+    } catch (e) {
+        return next(e);
+    }
 }
 
 module.exports = authenticateUser;
